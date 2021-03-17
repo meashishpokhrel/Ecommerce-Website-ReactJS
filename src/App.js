70c0a95fc4a9c048427631640fe2b8b951ea0679
meashishpokhrel <meashishpokhrel@gmail.com>
2021-03-17 07:18:10 +0545

    withRouter + linking Routes

--- src/App.js
+++ src/App.js
@@ -5,36 +5,19 @@ import {Route, Switch, Link} from "react-router-dom";
 
 
 const HatsPage = (props) => (
-  
   <div>
-    
-    <button onClick = { () => { props.history.push("/hats/list")}}> Next apge</button>
       <h1> Hats Page </h1>
   </div>
+  )
 
-)
 
-const Listme = () => (
-  <div>
-      <h1> This is Listings </h1>
-  </div>
-
-)
-const id = (props) => (
-  <div>
-      <h1> This is id turn {props.match.params.id} </h1>
-  </div>
-
-)
 
 function App() {
   return (
     <div>
       <Switch>
       <Route exact path="/" component = {Homepage} />
-      <Route exact path="/hats" component = {HatsPage} />
-      <Route exact path="/hats/list" component = {Listme} />
-      <Route path="/hats/list/:id" component = {id} />
+      <Route path="/hats" component = {HatsPage} />
       </Switch>
     </div>
     
--- src/component/menu-item/menu-item.component.jsx
+++ src/component/menu-item/menu-item.component.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 // import Directory from "../directory/directory.component";
 import "./menu-item.styles.scss"
+import {withRouter} from "react-router-dom";
 
-const MenuItem = ({title, imageUrl, size}) => (
-    <div className = {` ${size} menu-item`} >
+const MenuItem = ({title, imageUrl, size,history, match, linkedUrl}) => (
+    <div className = {` ${size} menu-item`} onClick = { () => history.push(`${match.url}${linkedUrl}`)}>
             <div className ="background-image" style={{ 
                  backgroundImage: `url(${imageUrl})` 
                     }} 
@@ -15,4 +16,4 @@ const MenuItem = ({title, imageUrl, size}) => (
             </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
