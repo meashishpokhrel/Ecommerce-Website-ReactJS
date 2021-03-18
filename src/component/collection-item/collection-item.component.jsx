b4958d17b00479e3e220e01183ace0fa99e9dd79
meashishpokhrel <meashishpokhrel@gmail.com>
2021-03-18 08:23:35 +0545

    Error corrected in Collection Item

--- src/component/collection-item/collection-item.component.jsx
+++ src/component/collection-item/collection-item.component.jsx
@@ -6,7 +6,7 @@ const CollectionItem = ({id, name, price, imageUrl}) => {
     return(
     <div className="collection-item">
         <div className="image" style={{backgroundImage: `url(${imageUrl})`}}/>
-        <div className="footer">
+        <div className="collection-footer">
             <span className="name">{name}</span>
             <span className="price">{price}</span>
         </div>
