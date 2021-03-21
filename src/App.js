e99738964ddfc638d3f4d50cc759798e77e44b31
meashishpokhrel <meashishpokhrel@gmail.com>
2021-03-21 20:38:40 +0545

    Sign In component with event handlers

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component"
 import {Route, Switch} from "react-router-dom";
 import Header from "./component/header/header.component";
+import signinAndsignup from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component"
 
 
 function App() {
@@ -11,7 +12,8 @@ function App() {
       <Header />
       <Switch>
       <Route exact path="/" component = {Homepage} />
-      <Route path="/shop" component = {ShopPage} />
+      <Route exact path="/shop" component = {ShopPage} />
+      <Route path="/signin" component = {signinAndsignup} />
       </Switch>
     </div>
     
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SignIn from "../../component/sign-in/sign-in.component"
+import "./sign-in-and-sign-up.styles.scss"
+
+const signinAndsignup = () =>{
+    return(
+        <div className="sign-in-and-sign-up">
+            <h1><SignIn /></h1>
+        </div>
+    )
+}
+
+export  default signinAndsignup;
