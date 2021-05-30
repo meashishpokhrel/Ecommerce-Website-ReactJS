a8e742d223559604c1f9155806ed8900aeb52e20
meashishpokhrel <meashishpokhrel@gmail.com>
2021-05-30 17:38:09 +0545

    Redux Concepts

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends React.Component {
 
     componentDidMount() {
       this.unsubscribeFromAuth=auth.onAuthStateChanged(async userAuth => {
-
+        
         if (userAuth){
           const userRef = await createUserProfileDocument(userAuth);
 
