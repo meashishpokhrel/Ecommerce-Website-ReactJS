c8c6f5084441c1b1bab79234e97f2c725f4fd4b0
meashishpokhrel <meashishpokhrel@gmail.com>
2021-06-01 20:24:47 +0545

    connect redux

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ class App extends React.Component {
   
   return (
     <div>
-      <Header currentUser={this.state.currentUser}/>
+      <Header/>
       
       <Switch>
       <Route exact path="/" component = {Homepage} />
--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -5,6 +5,7 @@ import { auth } from '../../firebase/firebase.utils';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
+import {connect} from "react-redux";
 import './header.styles.scss';
 
 const Header = ({ currentUser }) => (
@@ -32,4 +33,9 @@ const Header = ({ currentUser }) => (
   </div>
 );
 
-export default Header;
+
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser
+})
+
+export default connect()(Header);
