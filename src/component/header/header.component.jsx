51b3f64f977a04a39126654dde34dec30f356234
meashishpokhrel <meashishpokhrel@gmail.com>
2021-06-02 09:49:46 +0545

    Creating Cart Component

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -6,6 +6,7 @@ import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import {connect} from "react-redux";
+import CartIcon from "../cart-icon/cart-icon.component"
 import './header.styles.scss';
 
 const Header = ({ currentUser }) => (
@@ -29,6 +30,7 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
   </div>
 );
