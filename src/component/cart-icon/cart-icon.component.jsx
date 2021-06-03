bd6300d456da59a05c55b08d425be5bc42e6f838
meashishpokhrel <meashishpokhrel@gmail.com>
2021-06-03 22:08:16 +0545

    correction

--- src/component/cart-icon/cart-icon.component.jsx
+++ src/component/cart-icon/cart-icon.component.jsx
@@ -1,9 +1,11 @@
 import React from "react"
+
 import {connect} from "react-redux"
 
 import {ReactComponent as ShoppingIcon} from "../../assets/shopping-bag.svg"
 import { toggleCartHidden } from "../../redux/cart/cart-actions"
 import "./cart-icon.styles.scss"
+
 const CartIcon = ({toggleCartHidden}) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon  className="shopping-icon"/>
