c02992d9ede897effd7330751fc0c598f551068e
meashishpokhrel <meashishpokhrel@gmail.com>
2021-06-28 07:48:13 +0545

    Correction: Category renamed to Collection

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import CollectionItem from "../../component/collection-item/collection-item.component"
+import "./collection.styles.scss"
+
+const CollectionPage = ({match}) => {
+    return (
+    <div className="collection-page">
+        <h2>Collection Page</h2>
+    </div>
+)}
+
+export default CollectionPage
