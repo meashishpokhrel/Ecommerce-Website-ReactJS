788f795047e24ce30492616c2e9f1810cdbf312c
meashishpokhrel <meashishpokhrel@gmail.com>
2021-06-28 21:15:23 +0545

    selector and using ownProps

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import { connect } from "react-redux";
 import { Route } from "react-router-dom";
 import CollectionOverview from "../../component/collection-overview/collection-overview.component"
 import CollectionPage from "../collection/collection.component";
-
+import { selectCollection } from "../../redux/shop/shop-selector";
 
 const ShopPage = ({match}) => (
             <div className="shop-page">
@@ -13,4 +13,7 @@ const ShopPage = ({match}) => (
             </div>
 )
 
-export default ShopPage;
+const mapStateToProps = (state, ownProps) => {
+    collections: selectCollection(ownProps.match.params.collectionId)(state)
+}
+export default connect(mapStateToProps)(ShopPage);
