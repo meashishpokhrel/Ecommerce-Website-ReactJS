e5cb9b1e05b3f64386f44f6f7357022b91076404
meashishpokhrel <meashishpokhrel@gmail.com>
2021-07-31 20:42:00 +0545

    Bringing Data from firbase to the app

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,20 +4,25 @@ import { Route } from "react-router-dom";
 import {firestore, convertCollecSnapToMap} from "../../firebase/firebase.utils"
 import CollectionOverview from "../../component/collection-overview/collection-overview.component"
 import CollectionPage from "../collection/collection.component";
-
+import { connect } from "react-redux"
+import { updateCollection } from "../../redux/shop/shop-action";
 
 class ShopPage extends React.Component {
     unsubscribeFromSnapshot = null;
 
+
     componentDidMount(){
+        const {updateCollection}= this.props
         const collectionRef = firestore.collection("collections")
         
         collectionRef.onSnapshot(async snapshot => {
-            convertCollecSnapToMap(snapshot);
+            const collectionMap = convertCollecSnapToMap(snapshot);
+            updateCollection(collectionMap);
         })
     }
     render(){
         const {match} = this.props
+        
     return(
         <div className="shop-page">
             <Route exact path={`${match.path}`} component={CollectionOverview}/>
@@ -28,6 +33,9 @@ class ShopPage extends React.Component {
     }
 }
 
+const mapDispatchToProps = dispatch => ({
+    updateCollection: collectionMap => dispatch(updateCollection(collectionMap))
+})
 
 
-export default ShopPage;
+export default connect (null,mapDispatchToProps)(ShopPage);
