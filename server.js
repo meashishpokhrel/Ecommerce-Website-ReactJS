a9018f240d15fa18806c833cf8a711b88562e873
meashishpokhrel <meashishpokhrel@gmail.com>
2021-09-16 12:06:06 +0545

    Backedn for Stripe

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require ("path")
 
 if (process.env.NODE_ENV !== "production") require ("dotenv").config()
 
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 const app = express()
 const port = process.env.port || 5000
 
@@ -25,3 +26,19 @@ app.listen(port, err => {
     console.log("Listening on port"+ port);
 })
 
+app.post("/payment", (res,req) {
+    const body = {
+        source: req.body.token.id,
+        amount: req.body.amount,
+        currency: "usd"
+    }
+})
+
+stripe.charges.create(body, (strpErr, strpRes) => {
+    if (strpErr){
+        res.status(500).send ( {error: strpErr})
+    }
+    else{
+        res.status(200).send({success: strpRes})
+    }
+})
